f27d5dd037cd29e0871f67d356843b8f2b4319b0
Jaezmien Naejara <thenightalucard@gmail.com>
2020-12-21 10:54:27 +0800

    🥔 Uhh

--- build.bat
+++ build.bat
@@ -1,15 +1,5 @@
 @echo off
 
-set osType=%1
-:: if "%osType%"=="" (set osType=win)
-
-set binDir=bin/%osType%
-
-:: Clean previous builds
-echo [Build.bat] Cleaning...
-dotnet clean -c Release -o "%binDir%"
-dotnet clean -c Debug -o "%binDir%"
-
 :: Build
 echo [Build.bat] Building FiMSharp...
 dotnet build FiMSharp -c Release -o bin/
@@ -17,10 +7,14 @@ echo [Build.bat] Building FiMSharp.Javascript...
 dotnet build FiMSharp.Javascript -c Release -o bin/
 
 :: Build test program (optional)
+set osType=%1
 if not "%osType%"=="" (
     
     echo [Build.bat] Building test program...
-    set buildCommand=dotnet publish FiMSharp.Test --self-contained=true -p:PublishSingleFile=True -c Release -o "%binDir%/Playground"
+    set binDir=bin/%osType%
+    dotnet clean -c Release -o "%binDir%"
+    
+    set buildCommand=dotnet publish FiMSharp.Test --self-contained=true -p:PublishSingleFile=True -c Release -o "%binDir%"
 
     if "%osType%" == "win32" (
         %buildCommand% --runtime win-x86
