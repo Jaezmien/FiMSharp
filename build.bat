ba1f7ea745ff69ab35f54cd9365d34e320eb44c7
Jaezmien Naejara <thenightalucard@gmail.com>
2020-12-22 19:12:05 +0800

    🔨 Less suck

--- build.bat
+++ build.bat
@@ -1,35 +1,42 @@
 @echo off
+setlocal enabledelayedexpansion
 
 :: Build
 echo [Build.bat] Building FiMSharp...
 dotnet build FiMSharp -c Release -o bin/
+
+echo.
+echo.
 echo [Build.bat] Building FiMSharp.Javascript...
 dotnet build FiMSharp.Javascript -c Release -o bin/
 
 :: Build test program (optional)
-set osType=%1
-if not "%osType%"=="" (
+if not "%1"=="" (
     
-    echo [Build.bat] Building test program...
-    set binDir=bin/%osType%
-    dotnet clean -c Release -o "%binDir%"
-    
-    set buildCommand=dotnet publish FiMSharp.Test --self-contained=true -p:PublishSingleFile=True -c Release -o "%binDir%"
+    set FiMos=%1
+    set FiMbinDir=bin/!FiMos!
 
-    if "%osType%" == "win32" (
-        %buildCommand% --runtime win-x86
+    echo.
+    echo.
+    echo [Build.bat] Building test program to "!FiMbinDir!"...
+    dotnet clean -c Release -o "!FiMbinDir!"
+    
+    set buildCommand=dotnet publish FiMSharp.Test --self-contained=true -p:PublishSingleFile=True -c Release -o "!FiMbinDir!"
+
+    if "!FiMos!" == "win32" (
+        !buildCommand! --runtime win-x86
     )
-    if "%osType%" == "win" (
-        %buildCommand% --runtime win-x64
+    if "!FiMos!" == "win" (
+        !buildCommand! --runtime win-x64
     )
-    if "%osType%" == "linux" (
-        %buildCommand% --runtime linux-x64
+    if "!FiMos!" == "linux" (
+        !buildCommand! --runtime linux-x64
     )
-    if "%osType%" == "linuxarm" (
-        %buildCommand% --runtime linux-arm
+    if "!FiMos!" == "linuxarm" (
+        !buildCommand! --runtime linux-arm
     )
-    if "%osType%" == "darwin" (
-        %buildCommand% --runtime osx-x64
+    if "!FiMos!" == "darwin" (
+        !buildCommand! --runtime osx-x64
     )
 
 )
